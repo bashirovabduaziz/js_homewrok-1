43628805b817d4840852bee3d164bcd947f3b5de
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2023-06-07 18:17:30 +0500

    Homework 1

---
+++ index.js
@@ -0,0 +1,179 @@
+// let day = 1;
+// switch (day) {
+//   case 0:
+//     console.log('Sunday');
+//     break;
+//   case 1:
+//     console.log('Monday');
+//     break;
+//   case 2:
+//     console.log('Tuesday');
+//     break;
+//   case 3:
+//     console.log('Wednesday');
+//     break;
+//   case 4:
+//     console.log('Thursday');
+//     break;
+//   case 5:
+//     console.log('Friday');
+//     break;
+//   case 6:
+//     console.log('Saturday');
+//     break;
+//   default:
+//     console.log('Invalid day');
+// }
+
+// let mark = 5;
+// switch (mark) {
+//     case 1:
+//         console.log("yomon")
+//         break;
+//     case 2:
+//         console.log("qoniqarsiz")
+//         break;
+//     case 3:
+//         console.log("qoniqarli")
+//         break;
+//     case 4:
+//         console.log("yaxshi")
+//         break;
+//     case 5:
+//          console.log("a'lo")
+//         break;
+//     case 6:
+//         console.log("unaqa son yo'q")
+//         break;
+// }
+// let dayMonth = 2;
+// switch (dayMonth) {
+//     case 1:
+//         console.log("31 kun bor")
+//         break;
+//     case 2:
+//         console.log("28 kun bor")
+//         break;
+//     case 3:
+//         console.log("31 kun bor")
+//         break;
+//     case 4:
+//         console.log("30 kun bor")
+//         break;
+//     case 5:
+//         console.log("31 kun bor")
+//         break;
+//     case 6:
+//         console.log("30 kun bor")
+//         break;
+//     case 7:
+//         console.log("31 kun bor")
+//         break;
+//     case 8:
+//         console.log("31 kun bor")
+//         break;
+//     case 9:
+//         console.log("30 kun bor")
+//         break;
+//     case 10:
+//         console.log("31 kun bor")
+//         break;
+//     case 11:
+//         console.log("30 kun bor")
+//         break;
+//     case 12:
+//         console.log("31 kun bor")
+//         break;
+// }
+
+// let math = 4;
+// switch (math) {
+//     case 1:
+//         console.log("A+B")
+//         break;
+//     case 2:
+//         console.log("A-B")
+//         break;
+//     case 3:
+//         console.log("A%B")
+//         break;
+//     case 4:
+//         console.log("A*B")
+//         break;
+// }
+
+// let metr = 1;
+// switch (metr) {
+//     case 1:
+//         console.log("desimetr")
+//         break;
+//     case 2:
+//         console.log("kilometr")
+//         break;
+//     case 3:
+//         console.log("metr")
+//         break;
+//     case 4:
+//         console.log("millimetr")
+//         break;
+//     case 5:
+//         console.log("santimetr")
+//         break;
+// }
+// let gram = 1;
+// switch (gram) {
+//     case 1:
+//         console.log("gramm")
+//         break;
+//     case 2:
+//         console.log("kilogramm")
+//         break;
+//     case 3:
+//         console.log("milligramm")
+//         break;
+//     case 4:
+//         console.log("tonna")
+//         break;
+//     case 5:
+//         console.log("sentner")
+//         break;
+// }
+// let dayMonth = 2;
+// switch (dayMonth) {
+//     case 1:
+//         console.log("31 kun bor Yanvar")
+//         break;
+//     case 2:
+//         console.log("28 kun bor Fevral oyi")
+//         break;
+//     case 3:
+//         console.log("31 kun bor Mart oyi")
+//         break;
+//     case 4:
+//         console.log("30 kun bor Aprel oyi")
+//         break;
+//     case 5:
+//         console.log("31 kun bor May oyi")
+//         break;
+//     case 6:
+//         console.log("30 kun bor Iyun oyi")
+//         break;
+//     case 7:
+//         console.log("31 kun bor Iyul oyi")
+//         break;
+//     case 8:
+//         console.log("31 kun bor Avgust oyi")
+//         break;
+//     case 9:
+//         console.log("30 kun bor Sentabr oyi")
+//         break;
+//     case 10:
+//         console.log("31 kun bor Oktabr oyi")
+//         break;
+//     case 11:
+//         console.log("30 kun bor Noyabr")
+//         break;
+//     case 12:
+//         console.log("31 kun bor Dekabr")
+//         break;
+// }
